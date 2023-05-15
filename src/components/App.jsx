dbd8c17ab2a8156049039c4da6574701f68edd77
OleksandrLysenko777 <114435880+OleksandrLysenko777@users.noreply.github.com>
2023-05-15 19:06:42 +0200

    last

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,95 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList/ContactsList';
+import ContactsListElement from './ContactslistElement/ContactsListElement';
+import Filter from './Filter/Filter';
+import PropTypes from 'prop-types';
+
+export class App extends Component {
+  STORAGE_KEY = 'myContacts';
+  state = {
+    contacts: [
+       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  
+  componentDidMount() {
+    const persistedContacts = localStorage.getItem(this.STORAGE_KEY);
+
+    if (persistedContacts) {
+      this.setState({ contacts: JSON.parse(persistedContacts) });
+  
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(
+        this.STORAGE_KEY,
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
+
+
+  addContact = contact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  
+  };
+
+  deleteContact = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  filter = element => {
+    this.setState({
+      filter: element.currentTarget.value,
+    });
+  };
+
+  filterContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  render() {
+    const value = this.state.filter;
+    const filteredContacts = this.filterContacts();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          addContact={this.addContact}
+          contacts={this.state.contacts}
+        />
+        <h2>Contacts</h2>
+        <Filter
+          value={value}
+          filterContacts={this.filterContacts}
+          onChange={this.filter}
+        />
+        <ContactsList contactsLength={this.state.contacts.length}>
+          <ContactsListElement
+            contacts={filteredContacts}
+            deleteContact={this.deleteContact}
+          />
+        </ContactsList>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  filteredContacts: PropTypes.array,
 };
